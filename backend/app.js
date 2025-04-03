440543c07eb0c1b89d909d129170499a798c1fb1
hanzayaim <dummyargenta@gmail.com>
2025-04-03 19:36:41 +0700

    push for change app.js, add medical_handler.js, medical_routes.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const medicalRoutes = require("./medical/medical_routes");
 
 require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", medicalRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
