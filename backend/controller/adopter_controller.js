4374f5eafefec60c124c497608737512060eddd2
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-04-04 15:28:58 +0700

    commit adopter controller
    commit adopter controller

--- backend/controller/adopter_controller.js
+++ backend/controller/adopter_controller.js
@@ -1,11 +1,39 @@
 const pool = require("../config/db.js");
 
 // get data
-async function getAdopterData(id_shelter, adopter_id) {
+async function getAdopterDataById(id_shelter, id_adopter) {
   try {
     const [rows] = await pool.query(
       "SELECT * FROM adopter_profile where id_shelter = ? and id_adopter = ?",
-      [id_shelter, adopter_id]
+      [id_shelter, id_adopter]
+    );
+    if (rows.length > 0) {
+      return {
+        error: false,
+        message: "data fetched successfully",
+        data: rows[0],
+      };
+    } else {
+      return {
+        error: true,
+        message: "no data found",
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: true,
+      message: "error fetching data",
+      data: null,
+    };
+  }
+}
+
+async function getAdopterData(id_shelter) {
+  try {
+    const [rows] = await pool.query(
+      "SELECT * FROM adopter_profile where id_shelter = ?",
+      [id_shelter]
     );
     if (rows.length > 0) {
       return {
@@ -41,8 +69,8 @@ async function insertAdopterData(
   created_by
 ) {
   try {
-    await pool.query(
-      "INSERT INTO adopter_profile (id_shelter, name, profile_img, gender, phone_number, address, created_by) VALUES (?,?,?,?,?,?,?)",
+    const result = await pool.query(
+      "INSERT INTO adopter_profile (id_shelter, id_adopter,name, profile_img, gender, phone_number, address, created_by) VALUES (?,?,?,?,?,?,?)",
       [
         id_shelter,
         id_adopter,
@@ -57,7 +85,7 @@ async function insertAdopterData(
     return {
       error: false,
       message: "adopter created successfully",
-      data: adopter.adopter,
+      adopter: result.rows[0],
     };
   } catch (error) {
     return {
@@ -68,6 +96,70 @@ async function insertAdopterData(
   }
 }
 
+async function updateAdopterData(
+  adopter_name,
+  profile_img,
+  gender,
+  phone_number,
+  address,
+  updated_by,
+  updated_at,
+  id_shelter,
+  id_adopter
+) {
+  try {
+    const result = await pool.query(
+      "UPDATE adopter_profile SET name=?, profile_img=?, gender=?, phone_number=?, address=?, updated_by=?, updated_at=? WHERE id_shelter = ? and id_adopter = ?",
+      [
+        adopter_name,
+        profile_img,
+        gender,
+        phone_number,
+        address,
+        updated_by,
+        updated_at,
+        id_shelter,
+        id_adopter,
+      ]
+    );
+    return {
+      error: false,
+      message: "adopter updated successfully",
+      adopter: result.rows[0],
+    };
+  } catch (error) {
+    return {
+      error: true,
+      message: "error updating adopter",
+      data: null,
+    };
+  }
+}
+
+async function deleteAdopterData(id_shelter, id_adopter) {
+  try {
+    const result = await pool.query(
+      "DELETE FROM adopter_profile WHERE id_shelter = ? and id_adopter = ?",
+      [id_shelter, id_adopter]
+    );
+    return {
+      error: false,
+      message: "adopter deleted successfully",
+      adopter: result.rows[0],
+    };
+  } catch (error) {
+    return {
+      error: true,
+      message: "error deleting adopter",
+      data: null,
+    };
+  }
+}
+
 module.exports = {
   getAdopterData,
+  insertAdopterData,
+  getAdopterDataById,
+  updateAdopterData,
+  deleteAdopterData,
 };
