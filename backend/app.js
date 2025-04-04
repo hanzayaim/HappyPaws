8a8bc8ee3a4dda1aa6e02c90bd958f678386dd4d
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-04-04 15:55:16 +0700

    commit adopter routes
    commit adopter routes

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const routes = require("./routes");
+const port = process.env.PORT || 3000;
 
 require("dotenv").config();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
