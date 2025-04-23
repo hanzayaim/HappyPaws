4541cc91e6df67e861652af81fbad892a333968a
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-04-23 13:42:30 +0700

    login-page UI
    login-page UI

--- frontend/happy-paws/src/App.jsx
+++ frontend/happy-paws/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RegisterOwner from "./pages/register-owner";
 import RegisterPage from "./pages/register-page";
 import RegisterEmployee from "./pages/register-employee";
+import LoginPage from "./pages/login-page";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/register/register-owner" element={<RegisterOwner />} />
         <Route
