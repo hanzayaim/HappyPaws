03c294d94b33c00804c91d1f85cf76109e0217ab
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-05-08 17:47:32 +0700

    update timezone
    update timezone

--- frontend/happy-paws/src/components/pages-components/IncomeDialog.jsx
+++ frontend/happy-paws/src/components/pages-components/IncomeDialog.jsx
@@ -54,14 +54,14 @@ export function InsertIncomeDialog({ open, onOpenChange, User, fetchData }) {
           id_shelter: User.id_shelter,
           name: data.incomeName,
           amount: data.incomeAmount,
-          date: data.incomeDate,
+          date: data.incomeDate.toLocaleDateString("en-CA"),
           type: data.incomeType,
           note: data.incomeNote || "",
           created_by: User.owner_name ? User.owner_name : User.name,
         }
       );
       const result = response.data;
-      console.log(data.incomeDate);
+      console.log(data.incomeDate.toLocaleDateString("en-CA"));
       if (result.error) {
         throw new Error(result.message || "Failed to insert income data");
       }
