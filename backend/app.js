88ac391c154da538e383e0268aa701427c789993
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-05-13 21:57:48 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(
   cors({
-    origin: true,
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
