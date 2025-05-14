27206fce3d625a0909e79cd584d23c3587971c3d
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-05-14 15:20:02 +0700

    Update finance-management.jsx

--- frontend/happy-paws/src/pages/finance-management.jsx
+++ frontend/happy-paws/src/pages/finance-management.jsx
@@ -195,9 +195,12 @@ export default function FinancePage() {
       const SalaryRes = await axios.get(
         `/api/salary/getSalary/${userData.id_shelter}`
       );
+      if (SalaryRes.status === 404) {
+        setSalaries(null);
+        return;
+      }
       const SalaryData = SalaryRes.data;
       if (SalaryData.error) {
-        setSalaries([]);
         throw new Error(SalaryData.message || "Failed to fetch Salary");
       }
 
