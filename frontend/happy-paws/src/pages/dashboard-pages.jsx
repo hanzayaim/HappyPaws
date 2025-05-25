3062cd421686c2e9c3880afb4fcb10a3b212c7fa
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-05-25 14:29:00 +0700

    Update dashboard-pages.jsx

--- frontend/happy-paws/src/pages/dashboard-pages.jsx
+++ frontend/happy-paws/src/pages/dashboard-pages.jsx
@@ -172,6 +172,9 @@ export default function Dashboard() {
         };
       });
 
+      const totalAnimalCount = enrichedAnimalData.length;
+      setTotalAnimals(totalAnimalCount);
+
       setTotalAnimalsAdopted(
         enrichedAnimalData.filter(
           (animal) => animal.animal_status === "Adopted"
@@ -228,8 +231,6 @@ export default function Dashboard() {
             (medical) => medical.vaccin_status === "Not Vaccinated"
           ).length
       );
-
-      setTotalAnimals(enrichedAnimalData.length);
 
       setTotalMedicalRecords(
         medicalDataFetch.data.filter(
