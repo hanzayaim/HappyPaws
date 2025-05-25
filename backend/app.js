2def0437428701e422286f35c5fd74d2f79d3e1b
hanzayaim <dummyargenta@gmail.com>
2025-05-25 14:48:55 +0700

    push for fix app.js

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,12 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(
   cors({
-    origin: "https://happypawsshelter.netlify.app",
+    origin: [
+      "https://happypawsshelter.netlify.app",
+      "https://www.happypawsshelter.netlify.app",
+    ],
     credentials: true,
+    exposedHeaders: ["set-cookie"],
   })
 );
 
@@ -25,16 +29,23 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    proxy: true,
     cookie: {
       httpOnly: true,
       secure: true,
-      sameSite: 'none',
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
+      domain: ".happypawsshelter.netlify.app",
     },
   })
 );
 
 app.use("/api", routes);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Private-Network', 'true');
+  res.header('Cache-Control', 'no-store');
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
