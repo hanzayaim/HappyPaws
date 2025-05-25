dee76649da8a0dbd351344269f6815c33f5366aa
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-05-25 14:59:37 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,22 +3,18 @@ const app = express();
 const cors = require("cors");
 const session = require("express-session");
 const routes = require("./routes");
-const port = process.env.PORT || 3000;
 require("dotenv").config();
 
-// const isProduction = true;
+const port = process.env.PORT || 3000;
+const isProduction = true;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(
   cors({
-    origin: [
-      "https://happypawsshelter.netlify.app",
-      "https://www.happypawsshelter.netlify.app",
-    ],
+    origin: "https://happypawsshelter.netlify.app",
     credentials: true,
-    exposedHeaders: ["set-cookie"],
   })
 );
 
@@ -29,23 +25,17 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    proxy: true,
     cookie: {
       httpOnly: true,
-      secure: true,
-      sameSite: "none",
+      secure: isProduction,
+      sameSite: "lax",
+      domain: ".happypawsshelter.com",
       maxAge: 24 * 60 * 60 * 1000,
-      domain: ".happypawsshelter.netlify.app",
     },
   })
 );
 
 app.use("/api", routes);
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Private-Network', 'true');
-  res.header('Cache-Control', 'no-store');
-  next();
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
