6b9b749b9d12819e971acecaf78e08eb2b38f773
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-06-08 16:04:38 +0700

    Update data-convert.jsx

--- frontend/happy-paws/src/pages/data-convert.jsx
+++ frontend/happy-paws/src/pages/data-convert.jsx
@@ -422,8 +422,10 @@ export default function DataConvert() {
               {["Medical", "Owner"].includes(userData?.role) && (
                 <SelectItem value="medical">Medical</SelectItem>
               )}
-              <SelectItem value="animal">Animal</SelectItem>
               {["Administrator", "Owner"].includes(userData?.role) && (
+                <SelectItem value="animal">Animal</SelectItem>
+              )}
+              {["Finance", "Owner"].includes(userData?.role) && (
                 <SelectItem value="salary">Salary</SelectItem>
               )}
               {["Finance", "Owner"].includes(userData?.role) && (
