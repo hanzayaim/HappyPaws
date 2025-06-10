7de9cb5471d381b5fc44299af78e266fe103ec11
Fairuz Firnanda Arianto <ruzz.arianto@gmail.com>
2025-06-10 11:40:59 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -28,8 +28,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false,
-      sameSite: "Lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     },
   })
